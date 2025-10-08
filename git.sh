6fe5d57217e0d390c61416883f64464fd24b766b
ModelEarth <48058099+ModelEarth@users.noreply.github.com>
2025-10-08 03:43:51 -0400

    Update webroot

--- git.sh
+++ git.sh
@@ -2,6 +2,12 @@
 
 # Simple passthrough script to team/git.sh
 # Usage: ./git.sh [any arguments]
+#
+# Expected behavior when running "./git.sh push" from webroot:
+# - Commit all submodules of the webroot repo (including the team submodule)
+# - Commit extra repos that are not submodules  
+# - Commit the webroot repo itself
+# - Push all repositories with changes
 
 # Run here (in webroot) regardless of where called from
 cd "$(dirname "$0")"
@@ -13,5 +19,7 @@ if [ ! -f "team/git.sh" ]; then
     exit 1
 fi
 
-# Run team/git.sh from webroot directory (don't cd into team)
+# Run team/git.sh from webroot directory with proper context
+# Pass webroot path as environment variable so team/git.sh knows the context
+export WEBROOT_CONTEXT="$(pwd)"
 exec ./team/git.sh "$@"
